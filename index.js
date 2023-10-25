708259f94a2aae997b94da42d7693e9b1e0a1718
Maksim Gulay <maksimgulaj@gmail.com>
2023-10-25 14:12:06 +0300

    clean code

--- index.js
+++ index.js
@@ -1,49 +1,39 @@
-// index.js
-const List = require("./contacts")
-const { Command } = require('commander');
+const List = require("./contacts");
+const { Command } = require("commander");
 const program = new Command();
+
 program
-  .option('-a, --action <type>', 'choose action')
-  .option('-i, --id <type>', 'user id')
-  .option('-n, --name <type>', 'user name')
-  .option('-e, --email <type>', 'user email')
-  .option('-p, --phone <type>', 'user phone');
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
 program.parse(process.argv);
 
 const argv = program.opts();
 
-
-
-const invokeAction = async ({action, id , name, email, phone}) =>  {
-switch(action) {
-    case 'list':
-        const allContacts = await List.listContacts();
-        console.log(allContacts);
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await List.listContacts();
       break;
-    case 'get':
+
+    case "get":
       const oneContact = await List.getContactById(id);
-      console.log(oneContact);
       break;
 
-    case 'add':
+    case "add":
       const newContsct = await List.addContact(name, email, phone);
-      console.log(newContsct)
       break;
 
-    case 'remove':
+    case "remove":
       const contactDelite = await List.removeContact(id);
-      console.log(contactDelite)
       break;
 
     default:
-      console.warn('\x1B[31m Unknown action type!');
-    
-}
-}
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
 
 invokeAction(argv);
-// invokeAction({action: "list"})
-// invokeAction({action: "get", id: "qdggE76Jtbfd9eWJHrssH"})
-// invokeAction({action: "add", name: "Jon Dou", email: "djon@mail.com", phone: "(992) 96-7895"})
-// invokeAction({action: "remove", id: "OyJQnT6Sr55LN4uhdSIQh"})
